261f4e84a0f3f4e41bdada8cb53f9a7de3d9b2a6
Stéphane Bisinger <stephane@sbisinger.ch>
2022-09-17 14:59:50 +0200

    Show data for a single scenario

--- main.py
+++ main.py
@@ -36,13 +36,23 @@ def list_scenarios():
     return list(scenario_db.values())
 
 
+@app.get("/scenarios/{id}")
+def list_scenarios(id):
+    load_scenarios()
+    print(scenario_db)
+    print(scenario_db.get(id))
+    return scenario_db.get(id)
+
+
 def load_scenarios():
     global scenario_db
     if len(scenario_db) != 0:
         return
-    scenario_files = os.listdir("./data")
+    scenario_files = [
+        filename for filename in os.listdir("./data") if filename.endswith(".csv")
+    ]
     scenario_db = {
-        f"Scenario {i}": format_scenario(scenario)
+        scenario.replace(".csv", ""): format_scenario(scenario)
         for i, scenario in enumerate(scenario_files)
     }
 
@@ -51,9 +61,13 @@ def format_scenario(scenario_csv):
     df = pandas.read_csv(f"data/{scenario_csv}", header=None)
     print(df)
     print(df[df[0] == "Electricity|Imports"].squeeze()[1:])
+    name = scenario_csv.replace(".csv", "")
     return {
-        "name": f"Scenario {scenario_csv.replace('.csv', '')}",
+        "key": name,
+        "name": f"Scenario {name}",
         "co2": df[df[0] == "CO2|Total"].squeeze().iloc[1],
         "cost": df[df[0] == "Costs|System cost"].squeeze().iloc[1],
-        "domestic": df[df[0] == "Electricity|Imports"].squeeze()[1:].sum(),
+        "domestic": pandas.to_numeric(
+            df[df[0] == "Electricity|Imports"].squeeze()[1:], errors="ignore"
+        ).sum(),
     }
